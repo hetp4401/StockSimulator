da7ae97a3e22c3c07645f4937684672ee62e6e8b
unknown <patelhet32@gmail.com>
2020-06-03 20:26:34 -0400

    sample chart added

--- Components/Stock.js
+++ Components/Stock.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Button } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
 
 import { av_key } from "../config";
+
+import { LineChart } from "react-native-chart-kit";
 
 const Stock = ({ route, navigation }) => {
   const { ticker, name } = route.params.x;
@@ -31,9 +33,8 @@ const Stock = ({ route, navigation }) => {
 
   return (
     <View>
-        <View style= {{marginTop: 50}}></View>
+      <View style={{ marginTop: 50 }}></View>
       <Text>{ticker + " - " + name}</Text>
-      <Text>{response}</Text>
       <View style={{ alignItems: "center", marginTop: 30 }}>
         <TouchableOpacity onPress={() => get_intraday(1)}>
           <Text>1min</Text>
@@ -51,6 +52,50 @@ const Stock = ({ route, navigation }) => {
           <Text>60min</Text>
         </TouchableOpacity>
       </View>
+
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+              ],
+            },
+          ],
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFromOpacity: "#fb8c00",
+          backgroundGradientToOpacity: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726",
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
     </View>
   );
 };
