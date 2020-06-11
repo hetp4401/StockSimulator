0142553af1595977c6f9dc994a0da2fbb221bc89
unknown <patelhet32@gmail.com>
2020-06-11 02:29:49 -0400

    news scrape

--- Components/Stock.js
+++ Components/Stock.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Dimensions,
-  Alert,
-} from "react-native";
+import { View, Text, TouchableOpacity, Dimensions, Alert } from "react-native";
 
 import { av_key } from "../config";
 
 import { LineChart } from "react-native-chart-kit";
+
+const Parser = require("fast-html-parser");
 
 const Stock = ({ route, navigation }) => {
   const { ticker, name } = route.params.x;
@@ -21,6 +17,10 @@ const Stock = ({ route, navigation }) => {
   const [low, setlow] = useState("");
   const [volume, setvolume] = useState("");
   const [change, setchange] = useState("");
+
+  const [links, setlinks] = useState([]);
+
+  const [response, setresponse] = useState("trgrtg");
 
   const get_intraday = (interval) => {
     fetch(
@@ -47,9 +47,6 @@ const Stock = ({ route, navigation }) => {
         }
         setprices(processed_data);
         setslables(label_data);
-      })
-      .catch((error) => {
-        Alert.alert(error);
       });
   };
 
@@ -72,9 +69,23 @@ const Stock = ({ route, navigation }) => {
       });
   };
 
+  const get_news = () => {
+    fetch("https://www.bing.com/news/search?q=" + name)
+      .then((res) => res.text())
+      .then((body) => {
+        const html = Parser.parse(body);
+        const arr = html.querySelectorAll(
+          ".news-card.newsitem.cardcommon.b_cards2"
+        );
+        const links = arr.map((x) => x.rawAttributes.url);
+        console.log(links);
+      });
+  };
+
   useEffect(() => {
     get_intraday(5);
     get_quote();
+    get_news();
   }, []);
 
   return (
@@ -151,6 +162,8 @@ const Stock = ({ route, navigation }) => {
           {change}
         </Text>
       </View>
+
+      <Text>{response}</Text>
     </View>
   );
 };
